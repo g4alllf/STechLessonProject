8e3baa2fd0c6030091800a4c10d21e398342d903
g4allf <wuhan36@mail2.sysu.edu.cn>
2022-07-05 21:18:56 +0800

    "完成"

--- main.py
+++ main.py
@@ -15,10 +15,13 @@ for i in range(len1+1):
 for j in range(len2+1):
     dp[0][j] = j
 
+# i指向word1，j指向word2
 for i in range(1, len1+1):
     for j in range(1, len2+1):
+        # 如果字符匹配，就不需要进行额外操作
         if word1[i-1] == word2[j-1]:
             dp[i][j] = dp[i-1][j-1]
+        # 否则从增删改中找出操作数最少的一个增加一次操作
         else:
             dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1
 
